c34de6f106bb0cd83dd18acee036b2fa27283b30
denysdev1 <denys.kleimenov.dev@gmail.com>
2024-12-10 09:35:45 +0200

    Create sign up ui

---
+++ src/app/(logged-out)/sign-up/page.tsx
@@ -0,0 +1,239 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { CalendarIcon, PersonStandingIcon } from 'lucide-react';
+import Link from 'next/link';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/components/ui/form';
+import {
+  Select,
+  SelectValue,
+  SelectItem,
+  SelectContent,
+  SelectTrigger,
+} from '@/components/ui/select';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { Calendar } from '@/components/ui/calendar';
+import { format } from 'date-fns';
+
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    accountType: z.enum(['personal', 'company']),
+    companyName: z.string().optional(),
+    numberOfEmployees: z.coerce.number().optional(),
+    dob: z.date().refine((date) => {
+      const today = new Date();
+      const eighteenYearsAgo = new Date(
+        today.getFullYear() - 18,
+        today.getMonth(),
+        today.getDate()
+      );
+
+      return date <= eighteenYearsAgo;
+    }, 'You must be at least 18 years old'),
+  })
+  .superRefine((data, ctx) => {
+    if (data.accountType !== 'company') return;
+
+    if (!data.companyName) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['companyName'],
+        message: 'Company name is required',
+      });
+    }
+
+    if (data.numberOfEmployees !== undefined && data.numberOfEmployees < 1) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['numberOfEmployees'],
+        message: 'Number of employees must be greater than 0',
+      });
+    }
+  });
+
+const SignUpPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      accountType: 'personal',
+      companyName: '',
+      numberOfEmployees: 0,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  const accountType = form.watch('accountType');
+  const dobFromDate = new Date();
+  dobFromDate.setFullYear(dobFromDate.getFullYear() - 120);
+
+  return (
+    <>
+      <PersonStandingIcon size={50} />
+      <Card className='w-full max-w-sm'>
+        <CardHeader>
+          <CardTitle>Sign up</CardTitle>
+          <CardDescription>Sign up for your SupportMe account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+              <FormField
+                control={form.control}
+                name='email'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='john@doe.com'
+                        type='email'
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='accountType'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Account Type</FormLabel>
+                    <Select onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder='Select an account type' />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value='personal'>Personal</SelectItem>
+                        <SelectItem value='company'>Company</SelectItem>
+                      </SelectContent>
+                      <FormMessage />
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              {accountType === 'company' && (
+                <>
+                  <FormField
+                    control={form.control}
+                    name='companyName'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Company Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder='Company name' {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name='numberOfEmployees'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Employees</FormLabel>
+                        <FormControl>
+                          <Input
+                            type='number'
+                            min={0}
+                            placeholder='Employees'
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />{' '}
+                </>
+              )}
+              <FormField
+                control={form.control}
+                name='dob'
+                render={({ field }) => (
+                  <FormItem className='flex flex-col pt-2'>
+                    <FormLabel>Date of Birth</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant='outline'
+                            className='normal-case flex justify-between pr-1'
+                          >
+                            {field.value ? (
+                              <span>{format(field.value, 'PPP')}</span>
+                            ) : (
+                              <span>Pick a date</span>
+                            )}
+                            <CalendarIcon />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent align='start' className='w-full p-0'>
+                        <Calendar
+                          mode='single'
+                          defaultMonth={field.value}
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          initialFocus
+                          fixedWeeks
+                          weekStartsOn={1}
+                          fromDate={dobFromDate}
+                          toDate={new Date()}
+                          captionLayout='dropdown-buttons'
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type='submit' className='w-full'>
+                Sign Up
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className='justify-between gap-2'>
+          <small>Already have an account? </small>
+          <Button variant='outline' size='sm' asChild>
+            <Link href='/login'>Login</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+};
+
+export default SignUpPage;
