dfd03e70d730a32c05ea28d5a6e7c6dfd8bcbe68
denysdev1 <denys.kleimenov.dev@gmail.com>
2024-12-10 14:56:12 +0200

    Fix unused variable TS error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -63,8 +63,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className='h-4 w-4' />,
-        IconRight: ({ ...props }) => <ChevronRight className='h-4 w-4' />,
+        IconLeft: () => <ChevronLeft className='h-4 w-4' />,
+        IconRight: () => <ChevronRight className='h-4 w-4' />,
         Dropdown: ({ ...dropdownProps }) => {
           const { fromYear, fromMonth, fromDate, toYear, toMonth, toDate } =
             useDayPicker();
